63b1dff3ab88f419fdab8ddc0885246e0179a228
codeWithGz <giustt4@gmail.com>
2025-09-23 11:53:09 -0300

    Service layer, component registration

--- src/main/java/com/medicaloperations/MedOps/resources/ProfessionalResource.java
+++ src/main/java/com/medicaloperations/MedOps/resources/ProfessionalResource.java
@@ -1,21 +1,34 @@
 package com.medicaloperations.MedOps.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.medicaloperations.MedOps.entities.Professional;
-import com.medicaloperations.MedOps.entities.enums.AccountStatus;
+import com.medicaloperations.MedOps.services.ProfessionalService;
 
 @RestController
 @RequestMapping(value = "/professionals")
 public class ProfessionalResource {
 
+	@Autowired
+	private ProfessionalService service;
+
 	@GetMapping
-	public ResponseEntity<Professional> findAll(){
-		Professional p = new Professional(1L, "cody", "cody@gmail.com", "codyTheBest", AccountStatus.ACTIVE);
-		return ResponseEntity.ok().body(p);
+	public ResponseEntity<List<Professional>> findAll() {
+		List<Professional> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
-	
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Professional> findById(@PathVariable Long id) {
+		Professional obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
