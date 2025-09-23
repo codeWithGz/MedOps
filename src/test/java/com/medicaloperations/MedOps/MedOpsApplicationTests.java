d0a3e99e4a3021f616d3f84fa6f6c7be6bfefb45
codeWithGz <giustt4@gmail.com>
2025-09-23 10:21:15 -0300

    Initializing Project

---
+++ src/test/java/com/medicaloperations/MedOps/MedOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicaloperations.MedOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
