7b084c42e76260fb0dc54620bf3cf63735f3e288
codyWithGz <giustt4@gmail.com>
2025-12-18 17:52:25 -0300

    Fix: Populando o DB AWS se estiver vazio

--- src/main/java/com/medicaloperations/MedOps/config/TestConfig.java
+++ src/main/java/com/medicaloperations/MedOps/config/TestConfig.java
@@ -13,7 +13,7 @@ import com.medicaloperations.MedOps.entities.enums.Specialty;
 import com.medicaloperations.MedOps.repositories.DoctorRepository;
 
 @Configuration
-@Profile("test")
+@Profile({"test", "prod"})
 public class TestConfig implements CommandLineRunner {
 
 	@Autowired
@@ -24,14 +24,17 @@ public class TestConfig implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		Doctor d1 = new Doctor(null, "codeTheClinical", Specialty.CLINICO, "code@gmail.com", "codeTheClinical", AccountStatus.ACTIVE);
-		Doctor d2 = new Doctor(null, "codeThePharmacist", Specialty.FARMACEUTICO, "code@gmail.com", "codeThePharmacist",AccountStatus.ACTIVE);
-		Doctor d3 = new Doctor(null, "codeTheAuxiliar", Specialty.AUXENFERMAGEM, "code@gmail.com", "codeTheAuxiliar",AccountStatus.ACTIVE);
-		Doctor d4 = new Doctor(null, "codeTheNurse", Specialty.ENFERMEIRO, "code@gmail.com", "codeTheNurse",AccountStatus.ACTIVE);
-		Doctor d5 = new Doctor(null, "codeTheDentist", Specialty.DENTISTA, "code@gmail.com", "codeTheDentist",AccountStatus.ACTIVE);
-		professionalRepository.saveAll(Arrays.asList(d1, d2, d3, d4, d5));
-
+		
+		if (professionalRepository.count() == 0) {
+			
+			Doctor d1 = new Doctor(null, "codeTheClinical", Specialty.CLINICO, "code@gmail.com", "codeTheClinical", AccountStatus.ACTIVE);
+			Doctor d2 = new Doctor(null, "codeThePharmacist", Specialty.FARMACEUTICO, "code@gmail.com", "codeThePharmacist",AccountStatus.ACTIVE);
+			Doctor d3 = new Doctor(null, "codeTheAuxiliar", Specialty.AUXENFERMAGEM, "code@gmail.com", "codeTheAuxiliar",AccountStatus.ACTIVE);
+			Doctor d4 = new Doctor(null, "codeTheNurse", Specialty.ENFERMEIRO, "code@gmail.com", "codeTheNurse",AccountStatus.ACTIVE);
+			Doctor d5 = new Doctor(null, "codeTheDentist", Specialty.DENTISTA, "code@gmail.com", "codeTheDentist",AccountStatus.ACTIVE);
+			professionalRepository.saveAll(Arrays.asList(d1, d2, d3, d4, d5));
+			
+		}
 
 	}
 }
